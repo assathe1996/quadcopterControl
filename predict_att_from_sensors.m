d87c35ba1b2ea309467000485d15191ce59bec99
Tim McNamara <tmcnama2@andrew.cmu.edu>
2020-12-06 13:27:54 -0500

    Fixed some typos in prediction/comp filt tuning script

--- predict_att_from_sensors.m
+++ predict_att_from_sensors.m
@@ -1,5 +1,6 @@
 clear
 clc
+close all
 
 %sensor_data_file = "~/px4-19/Firmware/build/px4_sitl_default/tmp/rootfs/sensor_data.txt";
 sensor_data_file = "sensor_data.txt";
@@ -11,17 +12,17 @@ mag_x = sensor_data(:,2);
 mag_y = sensor_data(:,3);
 mag_z = sensor_data(:,4);
 
-imu_dt = sensor_data(:,5)/1e6;
-imu_p = sensor_data(:,6);
-imu_q = sensor_data(:,7);
-imu_r = sensor_data(:,8);
+imu_p = sensor_data(:,5);
+imu_q = sensor_data(:,6);
+imu_r = sensor_data(:,7);
+imu_dt = sensor_data(:,8);
 
 % seeing something very weird here
 % it looks as though acc_y is really acc_z
-acc_dt = sensor_data(:,9)/1e6;
-acc_x = sensor_data(:,10);
-acc_y = sensor_data(:,11);
-acc_z = sensor_data(:,12);
+acc_x = sensor_data(:,9);
+acc_y = sensor_data(:,10);
+acc_z = sensor_data(:,11);
+acc_dt = sensor_data(:,12);
 
 p_hat = zeros(N,1);
 q_hat = zeros(N,1);
@@ -36,11 +37,13 @@ ax = zeros(N,1);
 ay = zeros(N,1);
 az = zeros(N,1);
 
-acc_lpf_weight = 1;
-imu_lpf_weight = 1;
-mag_lpf_weight = 1;
-gyro_weight = .98;
-accel_weight = .02;
+acc_lpf_weight = .8;
+imu_lpf_weight = .8;
+mag_lpf_weight = .8;
+accel_weight = 0.001;
+gyro_weight = 1-accel_weight;
+mag_weight = .05;
+gyro_weight_heading = 1-mag_weight;
 
 for i = 2:N
     % LPF the data where applicable
@@ -58,8 +61,9 @@ for i = 2:N
     
     % estimate using the complementary filters
     dt = t_sensor(i) - t_sensor(i-1);
-    roll_from_acc =  atan2(ay(i), sqrt(ax(i)^2 + az(i))^2);
-    pitch_from_acc = atan2(-ax(i), sqrt(ay(i)^2 + az(i))^2);
+    %dt = imu_dt(i);
+    roll_from_acc =  atan2(ay(i), sqrt(ax(i)^2 + az(i)^2));
+    pitch_from_acc = atan2(-ax(i), sqrt(ay(i)^2 + az(i)^2));
     
     cf_p = p_hat(i); % could also use imu_p(i) if you don't want filtered value
     phi_hat(i) = gyro_weight*(phi_hat(i-1) + cf_p*dt) + accel_weight*roll_from_acc; 
@@ -77,7 +81,7 @@ for i = 2:N
     psi_from_mag = atan2(num, denom);
     %psi_from_mag = -atan2(my_norm, mx_norm);
     cf_r = psi_hat(i); % could also use imu_r(i) if you don't want filtered value
-    psi_hat(i) = gyro_weight*(psi_hat(i-1) + cf_r*dt) + accel_weight*psi_from_mag;
+    psi_hat(i) = gyro_weight_heading*(psi_hat(i-1) + cf_r*imu_dt(i)) + mag_weight*psi_from_mag;
 end
 
 
@@ -94,14 +98,36 @@ figure()
 subplot(3,1,1)
 plot(t_true, phi_true.*180/pi, t_sensor, phi_hat.*180/pi);
 legend('\phi ekf', '\phi est')
+grid on
+ylabel('degrees º')
 title('\phi')
 subplot(3,1,2)
 plot(t_true, th_true.*180/pi, t_sensor, th_hat.*180/pi);
+ylabel('degrees º')
+grid on
 legend('\theta ekf', '\theta est')
 title('\theta')
 subplot(3,1,3)
 plot(t_true, psi_true.*180/pi, t_sensor, psi_hat.*180/pi);
 legend('\psi ekf', '\psi est')
+ylabel('degrees º')
+grid on
 title('\psi')
 
+figure()
+subplot(3,1,1)
+plot(t_true, (phi_true-phi_hat)*180/pi);
+grid on
+ylabel('degrees º')
+title('\phi error')
+subplot(3,1,2)
+plot(t_true, (th_true-th_hat)*180/pi);
+ylabel('degrees º')
+grid on
+title('\theta error')
+subplot(3,1,3)
+plot(t_true, (psi_true-psi_hat)*180/pi);
+ylabel('degrees º')
+grid on
+title('\psi error')
 
